c20f750a1a6e6f549db6a2c1dba67f4db9fbb9ba
mitunda <peterbrian28@gmail.com>
2023-07-24 22:29:05 +0300

    completed assignment

--- src/components/ProjectItem.js
+++ src/components/ProjectItem.js
@@ -1,3 +1,41 @@
+// import React from "react";
+
+
+// function ProjectItem({ name, about, technologies }) {
+//   return (
+//     <div className="project-item">
+//       <h3>{name}</h3>
+//       <p>{about}</p>
+//       <div className="technologies">
+//         {/* render a <span> for each technology in the technologies array */}
+//       </div>
+//     </div>
+//   );
+// }
+
+// export default ProjectItem;
+
+
+// import React from "react";
+
+// function ProjectItem({ name, about, technologies }) {
+//   return (
+//     <div className="project-item">
+//       <h3>{name}</h3>
+//       <p>{about}</p>
+//       <div className="technologies">
+//         {/* Check if technologies is an array before using map */}
+//         {Array.isArray(technologies) &&
+//           technologies.map((technology, index) => (
+//             <span key={index}>{technology}</span>
+//           ))}
+//       </div>
+//     </div>
+//   );
+// }
+
+// export default ProjectItem;
+
 import React from "react";
 
 function ProjectItem({ name, about, technologies }) {
@@ -6,7 +44,11 @@ function ProjectItem({ name, about, technologies }) {
       <h3>{name}</h3>
       <p>{about}</p>
       <div className="technologies">
-        {/* render a <span> for each technology in the technologies array */}
+        {/* Check if technologies is an array before using map */}
+        {Array.isArray(technologies) &&
+          technologies.map((technology, index) => (
+            <span key={index}>{technology}</span>
+          ))}
       </div>
     </div>
   );
--- src/components/ProjectList.js
+++ src/components/ProjectList.js
@@ -1,12 +1,40 @@
+// import React from "react";
+// import ProjectItem from "./ProjectItem";
+
+// function ProjectList({ projects }) {
+//   console.log(projects);
+//   return (
+//     <div id="projects">
+//       <h2>My Projects</h2>
+//       <div id="project-list">
+//         {projects.map((project) => (
+//           <ProjectItem key={project.id} title={project.title} description={project.description} />
+//         ))}
+//       </div>
+//     </div>
+//   );
+// }
+
+// export default ProjectList;
+
 import React from "react";
 import ProjectItem from "./ProjectItem";
 
 function ProjectList({ projects }) {
-  console.log(projects);
   return (
     <div id="projects">
       <h2>My Projects</h2>
-      <div id="project-list">{/* render ProjectItem components here */}</div>
+      <div id="project-list">
+        {/* Map over the projects array and render ProjectItem components */}
+        {projects.map((project) => (
+          <ProjectItem
+            key={project.id}
+            name={project.name}
+            about={project.about}
+            technologies={project.technologies}
+          />
+        ))}
+      </div>
     </div>
   );
 }
